db73ab9fb7564f3f9666ffb47e9cc709025714df
gobahessam <65198407+gobahessam@users.noreply.github.com>
2021-01-23 00:25:02 +0300

    Add files via upload

---
+++ hasham/app.js
@@ -0,0 +1,8 @@
+// Not interested to writed I Am Rochie everywhere , just enjoy the code 😃
+
+alert (" Note: 🎶 Tap anywhere to enable music 🎶 ,        This is a flip card" )
+
+window.addEventListener("mouseover",play);
+window.addEventListener("click",play);
+function play(){ document.getElementById("audio").play();
+}
